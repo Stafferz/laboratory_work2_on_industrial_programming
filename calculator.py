a068f743ee54de9cdc751e90a701d197c45d8149
Stafferz <yecgaaa10@gmail.com>
2026-05-29 00:58:51 +0300

    add sqrt

--- calculator.py
+++ calculator.py
@@ -1,10 +1,14 @@
 from typing import Union
+import math
 
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
+
+def sqrt(a):
+    return math.sqrt(a)
 
 
 def multiply(a, b):
