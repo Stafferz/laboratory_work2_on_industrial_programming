56fd1e927f33dd3862c53bc880aea9b701645dc0
Stafferz <yecgaaa10@gmail.com>
2026-05-29 00:45:24 +0300

    test: add simple demonstration

--- calculator.py
+++ calculator.py
@@ -13,3 +13,7 @@ def divide(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
+
+if __name__ == "__main__":
+    print(add(2,3), subtract(5,2), multiply(3,4), divide(10,2))
